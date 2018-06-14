9f6f46076a567e8e793163198647a7be41726011
kirin1218 <kirin1218@outlook.jp>
2018-06-14 16:00:01 +0900

    making csvManager class 3

--- csvManager.py
+++ csvManager.py
@@ -1,55 +1,89 @@
 #-*- coding:utf-8 -*-
 import sys
 import os
-def char(charactor):
-    ret =  charactor[0]
-    print(ret,type(ret))
-    return ret
-    
+import codecs
+
+   
 class csvManager:
     def __init__(self,path):
         self._path = path
         self._raw = None
         self._rows = []
+        self._size = 0
         self.open()
     
-    def getline(self,start,linedata,maxidx):
-        nextline = start+1
-        CR = char(b'\r')
-        LF = char(b'\n')
-        COMMA = char(b',')
-        i = start
+    def parse(self):
+        i = 0
+        length = len(self._raw)
         cells = []
         stkbuf = '' 
-        while i <= maxidx:
+        dquat = False
+        while True:
             data = self._raw[i]
-            if data == CR or data == LF:
-                cells.append(str(stkbuf))
-                stkbuf=''
-                while self._raw[i] == CR or self._raw[i] == LF:
-                    i+=1 
-                continue
-            elif data == COMMA:
-                cells.append(stkbuf.decode('shift-jis'))
-                stkbuf=''
+           
+            if dquat == True:
+                if data == '\"':
+                    # ファイルの終端
+                    if i + 1 >= length:
+                        cells.append(stkbuf)
+                        break
+                    #ダブルクォートの終端
+                    elif self._raw[i+1] == ',': 
+                        cells.append(stkbuf)
+                        stkbuf = ''
+                        dquat = False
+                        i+=1
+                    #ダブルクオートのエスケープ　まだ途中
+                    elif self._raw[i+1] == '\"':
+                        stkbuf += data
+                        i += 1
+                    #謎のダブルクォート
+                    else:
+                        stkbuf + data
+                else:
+                    stkbuf += data
+                i+=1
+                
+                if i >= length:
+                    if len(stkbuf) > 0:
+                        cells.append(stkbuf)
+                    if len(cells) > 0:
+                        self._rows.append(cells)
+                    break
             else:
-                stkbuf += chr(data)
-            i+=1
-        nextline = i
-        return nextline
-
+                if len(stkbuf) == 0:
+                    if data == '\"':
+                        dquat = True
+                        i+=1
+                        continue
+         
+                if (data == '\r' and self._raw[i+1] == '\n') or data == '\n':
+                    cells.append(stkbuf)
+                    stkbuf=''
+                    self._rows.append(cells)
+                    cells = []
+                    if data == '\r' and self._raw[i+1] == '\n':
+                        i+=1 
+                elif data == ',':
+                    cells.append(stkbuf)
+                    stkbuf=''
+                else:
+                    stkbuf += data
+                i+=1
+                if i >= length:
+                    if len(stkbuf) > 0:
+                        cells.append(stkbuf)
+                    if len(cells) > 0:
+                        self._rows.append(cells)
+                    break
+        return len(self._rows)
     
     def open(self):
-        #size = os.path.getsize(self._path)
-u        with open(self._path,"rb",'Shift_jis') as f:
+        self._size = os.path.getsize(self._path)
+        with codecs.open(self._path,"rb",'Shift_jis') as f:
             f.seek(0)
             self._raw = f.read()
-            print(self._raw)
-            size = len(self._raw)
-            i = 0
-            while i < size:
-                linedata = []
-                i = self.getline(i,linedata,size-1)
+            self.parse()
 
 if __name__ == '__main__':
     path = '.' + os.sep + 'Data' + os.sep + 'log.csv'
